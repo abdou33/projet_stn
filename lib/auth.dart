988a83fafbf8b55835ee0e601066acc2713a853d
hibbisaid <hibbiabdo33@gmail.com>
2023-04-12 13:54:15 +0100

    ..

--- lib/auth.dart
+++ lib/auth.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 // ignore: unused_import
 import 'package:flutter/material.dart';
 class Auth{
-  final FirebaseAuth_firebaseAuth.instance;
+  final FirebaseAuth_firebaseAuth.currentUser;
   User? get currentUser => _firebaseAuth.currentUser;
   Stream<User?>get authStateChange => _firebaseAuth.authStateChanges();
   Future<void>signInWithEmailAndPassword({
